201409e49faf884de25b620c3ae64d474febba36
salgxv <salvarela09@yahoo.com>
2025-04-22 13:32:41 -0700

    index

--- Develop/server/src/routes/index.ts
+++ Develop/server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { ticketRouter } from './ticket-routes.js';
-import { userRouter } from './user-routes.js';
+import authRoutes from './auth-routes.js';
+import apiRoutes from './api/index.js';
+import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
-router.use('/tickets', ticketRouter);
-router.use('/users', userRouter);
+router.use('/auth', authRoutes);
+// TODO: Add authentication to the API routes
+router.use('/api', authenticateToken, apiRoutes);
 
 export default router;
