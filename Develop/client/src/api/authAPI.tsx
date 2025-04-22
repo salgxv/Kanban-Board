7c02718c175398f6ccbc386d39aeade96a3d62d6
salgxv <salvarela09@yahoo.com>
2025-04-22 11:45:52 -0700

    auth

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -3,7 +3,7 @@ import { UserLogin } from "../interfaces/UserLogin";
 const login = async (userInfo: UserLogin): Promise<string> => {
   // TODO: make a POST request to the login route
   try {
-    const response = await fetch('/auth/login', {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -14,15 +14,14 @@ const login = async (userInfo: UserLogin): Promise<string> => {
     const data = await response.json();
 
     if (!response.ok) {
-      throw new Error('Login failed');
+      throw new Error(data.message || 'Login failed');
     }
 
-    return data;
+    return data; 
   } catch (err) {
-    console.log('Error from user login: ', err);
+    console.error('Error from user login:', err);
     return Promise.reject('Could not fetch user info');
   }
-}
-
+};
 
 export { login };
